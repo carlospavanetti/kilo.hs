7b11c18d59f9640d9a7e8bfe9d2e07beb5103191
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2019-04-02 21:46:57 -0300

    Turn off echoing while typing

--- kilo.hs
+++ kilo.hs
@@ -2,8 +2,16 @@ module Main where
 
 import Control.Monad (when)
 import System.Posix.IO
+import System.Posix.Terminal
+
+enableRawMode :: IO ()
+enableRawMode = do
+    current <- getTerminalAttributes stdInput
+    let newAttributes = withoutMode current EnableEcho
+    setTerminalAttributes stdInput newAttributes WhenFlushed
 
 main :: IO()
 main = do
+    enableRawMode
     (char, count) <- fdRead stdInput 1
     when (count == 1 && char /= "q" ) main
