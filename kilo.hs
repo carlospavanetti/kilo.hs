08c152140099aa0c2d5f848d24efa4530ef3dac5
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2019-04-02 22:06:05 -0300

    Disable raw mode at exit

--- kilo.hs
+++ kilo.hs
@@ -4,14 +4,19 @@ import Control.Monad (when)
 import System.Posix.IO
 import System.Posix.Terminal
 
-enableRawMode :: IO ()
+enableRawMode :: IO TerminalAttributes
 enableRawMode = do
     current <- getTerminalAttributes stdInput
     let newAttributes = withoutMode current EnableEcho
     setTerminalAttributes stdInput newAttributes WhenFlushed
+    return current
+
+disableRawMode :: TerminalAttributes -> IO ()
+disableRawMode attrs = setTerminalAttributes stdInput attrs WhenFlushed
 
 main :: IO()
 main = do
-    enableRawMode
+    originalAttributes <- enableRawMode
     (char, count) <- fdRead stdInput 1
     when (count == 1 && char /= "q" ) main
+    disableRawMode originalAttributes
