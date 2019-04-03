11abe561a3e0b7c996927a9b760f8d83ee9e1312
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2019-04-02 21:24:22 -0300

    Read keys from stdin

--- kilo.hs
+++ kilo.hs
@@ -1,4 +1,10 @@
 module Main where
 
+import System.Posix.IO
+
 main :: IO()
-main = undefined
+main = do
+    (char, count) <- fdRead stdInput 1
+    if count == 1 then
+        main
+    else return ()
