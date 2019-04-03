50a02b6b567e9a22e122fa9ef3b9daedaa9ed588
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2019-04-02 22:32:31 -0300

    Fix Ctrl-M

--- kilo.hs
+++ kilo.hs
@@ -14,6 +14,7 @@ enableRawMode = do
             flip withoutMode KeyboardInterrupts $
             flip withoutMode StartStopOutput $
             flip withoutMode ExtendedFunctions $
+            flip withoutMode MapCRtoLF $
             current
     setTerminalAttributes stdInput newAttributes WhenFlushed
     return current
